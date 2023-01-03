e5dd079ff7fd6164cf30a2591f371e91d51e1727
BarkMulcher <lucas.c.haskell@gmail.com>
2023-01-03 12:56:26 -0500

    add basePath to config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,17 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? '/lettercutter/' : '',
+  basePath: '/lettercutter',
+  assetPrefix: isProd ? '/lettercutter/' : '/lettercutter/',
   images: {
     unoptimized: true,
   },
   reactStrictMode: true,
 }
 
-module.exports = nextConfig, {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
-}
+module.exports = nextConfig
+
+// {
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+// }
