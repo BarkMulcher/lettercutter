87c44be95a2ab63ca7d68dcd6ed27cc6a1116e17
BarkMulcher <lucas.c.haskell@gmail.com>
2023-01-03 16:02:03 -0500

    edit yml workflow

--- next.config.js
+++ next.config.js
@@ -12,9 +12,7 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
-
-// {
-  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
-// }
+module.exports = nextConfig, {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+}
