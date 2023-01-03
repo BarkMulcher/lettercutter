d11fef669e4430db82bc3bf717d32236774896cb
BarkMulcher <lucas.c.haskell@gmail.com>
2023-01-03 12:23:15 -0500

    edit config.js & yml & build

--- next.config.js
+++ next.config.js
@@ -9,4 +9,7 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig, {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+}
