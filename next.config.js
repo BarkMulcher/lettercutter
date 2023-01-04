571a294b25935f17a4e87fec40a88c77a079dfeb
BarkMulcher <lucas.c.haskell@gmail.com>
2023-01-03 19:03:32 -0500

    add comments to cfg.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/LetterCutter",
+  // this is important:
+    // changes to this returned the site to showing readme as the page
   assetPrefix: isProd ? "https://cdn.statically.io/gh/BarkMulcher/BarkMulcher.github.io/LetterCutter/gh-pages/" : '',
   images: {
     loader: "akamai",
