55a626d29043913458d45236629a39e2e9f68ea7
BarkMulcher <lucas.c.haskell@gmail.com>
2023-01-04 15:32:51 -0500

    rebuild with bootstrap test

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 // import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css';
 // import { useEffect } from 'react';
 import Layout from '../components/Layout';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
 
-  // useEffect(() => {
-  //   import('bootstrap/dist/js/bootstrap');
-  // })
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap');
+  }, []);
 
   return (
     <Layout>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import { Inter } from '@next/font/google';
-import '../styles/Header.module.css';
 
 
 
