461fd8cc1ffbd7f2a2602ecd116712d667a08c6b
BarkMulcher <lucas.c.haskell@gmail.com>
2023-01-12 15:55:47 -0500

    rebuild w/ lowercase basePath

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/LetterCutter",
+  basePath: "/lettercutter",
   // this is important:
     // changes to this returned the site to showing readme as the page
-  assetPrefix: isProd ? "https://BarkMulcher.github.io/LetterCutter" : '',
+  assetPrefix: isProd ? "https://BarkMulcher.github.io/lettercutter" : '',
   images: {
     loader: "akamai",
     path: "",
